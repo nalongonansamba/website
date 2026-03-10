9f8a23112419a760c24724f82613642a6fa11576
la-niina <la.niina.me@gmail.com>
2026-03-10 19:48:43 +0300

    feat: update admin logo image source to banner.

--- src/components/admin-logo/logo.tsx
+++ src/components/admin-logo/logo.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 export default function Logo() {
   return (
     <div className="flex items-center space-x-2">
-      <Image alt={'Logo'} fill className={'max-w-full shadow-2xl'} src={'/favicon.svg'} />
+      <Image
+        alt={'Logo'}
+        fill
+        className={'max-w-full shadow-2xl'}
+        src={'/nalongo-nansamba-banner.svg'}
+      />
     </div>
   )
 }
